250414c38fd3854e21a7ed11befb95fcafedbdc6
Shajadulhaqueemon111 <mdshajdulhaqueemon@gmail.com>
2025-05-21 17:17:05 +0600

    adde a register doctor is updated

--- src/components/Pages/OnlineDoctor/OnlineDoctors.tsx
+++ src/components/Pages/OnlineDoctor/OnlineDoctors.tsx
@@ -3,6 +3,7 @@ const OnlineDoctors = () => {
     <div>
       <h1>This is Online Doctor Page</h1>
       <p>Register doctor comming soon.....</p>
+      <p>Rgister doctor is updated</p>
     </div>
   );
 };
