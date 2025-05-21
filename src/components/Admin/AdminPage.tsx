3372599a4b1dd6a73dca6b1adfb70d6a8ad7cb11
Shajadulhaqueemon111 <mdshajdulhaqueemon@gmail.com>
2025-05-21 21:09:34 +0600

    add admin panel

--- src/components/Admin/AdminPage.tsx
+++ src/components/Admin/AdminPage.tsx
@@ -1,10 +1,10 @@
-
 const AdminPage = () => {
-    return (
-        <div>
-            <h1>Admin page ongoing... coming soon</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Admin page ongoing... coming soon</h1>
+      <h1>This is Admin panel </h1>
+    </div>
+  );
 };
 
 export default AdminPage;
